35392e2e63e3bca7e8030945333200e1b85b8fc3
erandro <erandro@gmail.com>
2018-09-28 20:02:01 -0500

    added all files

---
+++ models/Game.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var GameSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  catagory: {
+    type: String,
+    required: true
+  },
+  imgUrl: {
+    type: String,
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Game = mongoose.model("Game", GameSchema);
+
+module.exports = Game;
